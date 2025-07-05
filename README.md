230200f1d4200c9e4036354ef1d2e2d532acc745
Mohan Lal <mhnuk2007@gmail.com>
2025-07-05 10:24:36 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# HibProj2
---
+++ src/main/java/com/learning/Alien.java
@@ -0,0 +1,50 @@
+package com.learning;
+
+public class Allien {
+    private int aid;
+    private String aname;
+    private String tech;
+    private Laptop laptop;
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    @Override
+    public String toString() {
+        return "Allien{" +
+                "aid=" + aid +
+                ", aname='" + aname + '\'' +
+                ", tech='" + tech + '\'' +
+                ", laptop=" + laptop +
+                '}';
+    }
+}
